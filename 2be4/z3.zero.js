dc175cb293c41e98bb46eb71901285f2816dff95
iki1uc <entertrainer@web.de>
2026-06-07 18:07:22 +0200

    Update z3.zero.js

--- 2be4/z3.zero.js
+++ 2be4/z3.zero.js
@@ -1,1 +1,29 @@
+// z3.zero.js
+// ID: Z3-0CORE-01
+
+const ZeroThreeZero = {
+    id: "Z3-0CORE-01",
+    zero: 0,
+    one: 1,
+    activeZero: "Z3-0",
+    activeOne: "Z3-1",
+
+    getZero() {
+        return this.activeZero;
+    },
+
+    getOne() {
+        return this.activeOne;
+    },
+
+    setZero(id) {
+        this.activeZero = id;
+    },
+
+    setOne(id) {
+        this.activeOne = id;
+    }
+};
+
+export default ZeroThreeZero;
 
